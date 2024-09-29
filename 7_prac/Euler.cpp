86cfa62ea84f1c9d3b528b7edbbb49e7358cd7c2
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-09-29 12:03:28 +0300

    Update Euler.cpp

--- 7_prac/Euler.cpp
+++ 7_prac/Euler.cpp
@@ -1,17 +1,16 @@
-// A C++ program print Eulerian Trail in a given Eulerian or
-// Semi-Eulerian Graph
+// Программа на C++ для печати эйлерова пути в заданном эйлеровом или полуэйлеровом графе
 #include <algorithm>
 #include <iostream>
 #include <list>
 #include <string.h>
 using namespace std;
 
-// A class that represents an undirected graph
+// Класс, представляющий неориентированный граф
 class Graph {
-    int V; // No. of vertices
-    list<int>* adj; // A dynamic array of adjacency lists
+    int V; // Количество вершин
+    list<int>* adj; // Динамический массив списков смежности
 public:
-    // Constructor and destructor
+    // Конструктор и деструктор
     Graph(int V)
     {
         this->V = V;
@@ -19,33 +18,30 @@ public:
     }
     ~Graph() { delete[] adj; }
 
-    // functions to add and remove edge
+    // Функции для добавления и удаления ребер
     void addEdge(int u, int v)
     {
+        // Добавляем ребро между вершинами u и v
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     void rmvEdge(int u, int v);
 
-    // Methods to print Eulerian tour
+    // Методы для печати эйлерова пути
     void printEulerTour();
     void printEulerUtil(int s);
 
-    // This function returns count of vertices reachable
-    // from v. It does DFS
+    // Функция, возвращающая количество достижимых вершин из v. Использует DFS
     int DFSCount(int v, bool visited[]);
 
-    // Utility function to check if edge u-v is a valid next
-    // edge in Eulerian trail or circuit
+    // Вспомогательная функция для проверки, является ли ребро u-v допустимым следующим ребром в эйлеровом пути или контуре
     bool isValidNextEdge(int u, int v);
 };
 
-/* The main function that print Eulerian Trail. It first
-finds an odd degree vertex (if there is any) and then
-calls printEulerUtil() to print the path */
+/* Основная функция, печатающая эйлеров путь. Сначала она находит вершину с нечетной степенью (если такая есть), а затем вызывает printEulerUtil() для печати пути */
 void Graph::printEulerTour()
 {
-    // Find a vertex with odd degree
+    // Находим вершину с нечетной степенью
     int u = 0;
     for (int i = 0; i < V; i++)
         if (adj[i].size() & 1) {
@@ -53,21 +49,20 @@ void Graph::printEulerTour()
             break;
         }
 
-    // Print tour starting from oddv
+    // Печатаем путь, начиная с вершины с нечетной степенью
     printEulerUtil(u);
     cout << endl;
 }
 
-// Print Euler tour starting from vertex u
+// Печатаем эйлеров путь, начиная с вершины u
 void Graph::printEulerUtil(int u)
 {
-    // Recur for all the vertices adjacent to this vertex
+    // Рекурсивно обходим все вершины, смежные с этой вершиной
     list<int>::iterator i;
     for (i = adj[u].begin(); i != adj[u].end(); ++i) {
         int v = *i;
 
-        // If edge u-v is not removed and it's a a valid
-        // next edge
+        // Если ребро u-v не удалено и оно является допустимым следующим ребром
         if (v != -1 && isValidNextEdge(u, v)) {
             cout << u << "-" << v << " ";
             rmvEdge(u, v);
@@ -76,15 +71,13 @@ void Graph::printEulerUtil(int u)
     }
 }
 
-// The function to check if edge u-v can be considered as
-// next edge in Euler Tout
+// Функция для проверки, является ли ребро u-v допустимым следующим ребром в эйлеровом пути или контуре
 bool Graph::isValidNextEdge(int u, int v)
 {
-    // The edge u-v is valid in one of the following two
-    // cases:
+    // Ребро u-v является допустимым в одном из следующих двух случаев:
 
-    // 1) If v is the only adjacent vertex of u
-    int count = 0; // To store count of adjacent vertices
+    // 1) Если v является единственной смежной вершиной u
+    int count = 0; // Для хранения количества смежных вершин
     list<int>::iterator i;
     for (i = adj[u].begin(); i != adj[u].end(); ++i)
         if (*i != -1)
@@ -92,51 +85,47 @@ bool Graph::isValidNextEdge(int u, int v)
     if (count == 1)
         return true;
 
-    // 2) If there are multiple adjacents, then u-v is not a
-    // bridge Do following steps to check if u-v is a bridge
+    // 2) Если есть несколько смежных вершин, то u-v не является мостом. Выполняем следующие шаги для проверки, является ли u-v мостом
 
-    // 2.a) count of vertices reachable from u
+    // 2.a) Количество достижимых вершин из u
     bool visited[V];
     memset(visited, false, V);
     int count1 = DFSCount(u, visited);
 
-    // 2.b) Remove edge (u, v) and after removing the edge,
-    // count vertices reachable from u
+    // 2.b) Удаляем ребро (u, v) и после удаления ребра считаем количество достижимых вершин из u
     rmvEdge(u, v);
     memset(visited, false, V);
     int count2 = DFSCount(u, visited);
 
-    // 2.c) Add the edge back to the graph
+    // 2.c) Добавляем ребро обратно в граф
     addEdge(u, v);
 
-    // 2.d) If count1 is greater, then edge (u, v) is a
-    // bridge
+    // 2.d) Если count1 больше, то ребро (u, v) является мостом
     return (count1 > count2) ? false : true;
 }
 
-// This function removes edge u-v from graph. It removes
-// the edge by replacing adjacent vertex value with -1.
+// Функция для удаления ребра u-v из графа. Удаляет ребро, заменяя значение смежной вершины на -1.
 void Graph::rmvEdge(int u, int v)
 {
-    // Find v in adjacency list of u and replace it with -1
+    // Находим v в списке смежности u и заменяем его на -1
     list<int>::iterator iv
             = find(adj[u].begin(), adj[u].end(), v);
     *iv = -1;
 
-    // Find u in adjacency list of v and replace it with -1
+    // Находим u в списке смежности v и заменяем его на -1
     list<int>::iterator iu
             = find(adj[v].begin(), adj[v].end(), u);
     *iu = -1;
 }
 
-// A DFS based function to count reachable vertices from v
+// DFS-реализация для подсчета достижимых вершин из v
 int Graph::DFSCount(int v, bool visited[])
 {
-    // Mark the current node as visited
+    // Помечаем текущую вершину как посещенную
     visited[v] = true;
     int count = 1;
 
-    // Recur for all vertices adjacent to this vertex
+    // Рекурсивно обходим все вершины, смежные с этой вершиной
     list<int>::iterator i;
     for (i = adj[v].begin(); i != adj[v].end(); ++i)
         if (*i != -1 && !visited[*i])
@@ -145,11 +134,10 @@ int Graph::DFSCount(int v, bool visited[])
     return count;
 }
 
-// Driver program to test above function
+// Основная функция для тестирования вышеуказанных функций
 int main()
 {
-    // Let us first create and test graphs shown in above
-    // figure
+    // Сначала создаем и тестируем графы, показанные на диаграмме выше
     Graph g1(10);
     g1.addEdge(0, 1);
     g1.addEdge(0, 3);
@@ -168,7 +156,6 @@ int main()
     g1.addEdge(7, 9);
     g1.addEdge(8, 9);
     g1.printEulerTour();
-
 
     return 0;
 }
