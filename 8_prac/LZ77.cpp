df3a72039a2c85a79523d8ea1b4abcda5ac76620
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-10-10 10:42:39 +0300

    Update LZ77.cpp

--- 8_prac/LZ77.cpp
+++ 8_prac/LZ77.cpp
@@ -6,58 +6,73 @@
 
 using namespace std;
 
+// Структура для хранения кодированных данных
 struct CodeNode
 {
-    unsigned char beg;
-    unsigned char len;
-    char ch;
+    unsigned char beg; // смещение от начала окна поиска
+    unsigned char len; // длина повторяющейся подстроки
+    char ch; // текущий символ
 };
 
-bool push_shift (string& s, char c, size_t len)  // return true if first symbol were erased
+// Функция для сдвига символа в строке
+// Она используется для сдвига символа в окне поиска, когда мы находим повторяющуюся подстроку
+bool push_shift (string& s, char c, size_t len)  // возвращает true, если первый символ был удален
 {
     if (s.size() < len) {s.push_back(c); return false;}
+    // сдвигаем символы в строке, чтобы освободить место для нового символа
     move (next(s.begin()), s.end(), s.begin());
     s.back() = c; return true;
 }
 
+// Функция для кодирования строки с помощью алгоритма LZ77
 forward_list<CodeNode> LZ77 (string_view s, size_t win_len = 255)
 {
     forward_list<CodeNode> res; auto it = res.before_begin();
     string win, buf; win.reserve(win_len); buf.reserve(win_len);
     CodeNode next; size_t saved_win_len = 0;
+    // проходимся по каждому символу в строке
     for (char c : s) {
         buf.push_back(c);
         size_t pos;
         next.ch = c;
-//cout<<win<<' '<<buf<<' '<<saved_win_len<<'\n';
+        // ищем повторяющуюся подстроку в окне поиска
         if ((pos = win.rfind(buf)) != string::npos) {
+            // если нашли, то записываем смещение и длину повторяющейся подстроки
             next.beg = saved_win_len-pos; next.len = buf.size();
-            if (  push_shift (win, c, win_len)  ) saved_win_len--;  //shift
+            if (  push_shift (win, c, win_len)  ) saved_win_len--;  // сдвиг символа в окне поиска
         } else {
+            // если не нашли, то записываем новый символ и обнуляем буфер
             it = res.insert_after(it, next); buf.resize(0);
             next.beg = 0; next.len = 0;
             push_shift (win, c, win_len);
             saved_win_len = win.size();
         }
     }
+    // если последний символ не был записан, то записываем его
     if (next.len != 0) {next.len--; res.insert_after(it, next);}
     return res;
 }
 
-size_t LZ77length (const forward_list<CodeNode>& code) { // get length of original string
+// Функция для получения длины оригинальной строки
+size_t LZ77length (const forward_list<CodeNode>& code) { 
     size_t len = 0;
+    // проходимся по каждому кодированному символу и суммируем длины повторяющихся подстрок
     for (const CodeNode& cn : code) len += cn.len+1;
     return len;
 }
 
-size_t LZ77size (const forward_list<CodeNode>& code) {  // get size of coded data
+// Функция для получения размера кодированных данных
+size_t LZ77size (const forward_list<CodeNode>& code) {  
+    // размер кодированных данных равен размеру структуры CodeNode, умноженному на количество кодированных символов
     return sizeof(CodeNode) * distance(code.begin(), code.end());
 }
 
+// Функция для декодирования кодированных данных
 string LZ77decode (const forward_list<CodeNode>& code)
 {
     string res;
-    res.reserve(LZ77length(code)); // can not be
+    res.reserve(LZ77length(code)); // зарезервировать память для результата
+    // проходимся по каждому кодированному символу и восстанавливаем оригинальную строку
     for (CodeNode cn : code) {
         for (size_t i = res.size()-cn.beg, e = i+cn.len; i != e; ++i)
             res += res[i];
@@ -66,6 +81,7 @@ string LZ77decode (const forward_list<CodeNode>& code)
     return res;
 }
 
+// Оператор вывода для структуры CodeNode
 ostream& operator<< (ostream& os, CodeNode cn) {
     return os << '<' << int(cn.beg) << ',' << int(cn.len) << ',' << cn.ch << '>';
 }
@@ -75,9 +91,7 @@ int main()
     string s;
     s = "010110110110100010001"; // для тестирования
     auto code = LZ77(s,10);
+    // выводим кодированные данные
     for (CodeNode cn : code) cout << cn << ' ';
     cout << endl;
-    cout << LZ77decode(code) << '\n' << s << endl;
-    //cout << s.size() << ' ' << LZ77size(code) << endl;
-    return 0;
-}
+    // декодируем кодированные данные и выводим оригина
